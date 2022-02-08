42f098257c99fa586d03432ab08f53519fdb8085
viniciusmarquesvaz <80642632+viniciusmarquesvaz@users.noreply.github.com>
2022-02-08 20:55:26 -0300

    alteração formulário e cadastro
    alteração total da interface de cadastro
    e Login, e vinculação com o backend , armazenando usuários no banco e validando entrada no ambiente do sistema

---
+++ scr/static/index.js
@@ -0,0 +1,15 @@
+$('.message a').click(function () {
+  $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow')
+})
+//função para botão esqueci a senha
+
+
+// ;(function () {
+//   var dialog = document.getElementById('myFirstDialog')
+//   document.getElementById('show').onclick = function () {
+//     dialog.show()
+//   }
+//   document.getElementById('hide').onclick = function () {
+//     dialog.close()
+//   }
+// })()
