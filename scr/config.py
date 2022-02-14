32066bb5eafca91c054c081aa376b766d02ffe61
Pedro <phenrique.alvesilva@gmail.com>
2022-02-14 00:19:08 -0300

    Conectando a database Heroku
    Conectando a database do Heroku

--- scr/config.py
+++ scr/config.py
@@ -1,3 +1,3 @@
 SECRET_KEY = "secreto"
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:teamo123@localhost/app-tangram"
+SQLALCHEMY_DATABASE_URI = "postgres://kjpagsnneweohn:bc8265fb587d3d21fa6d7ce1120a6b77ef20806bfea102d0318f2b1cf4bfa486@ec2-44-194-113-156.compute-1.amazonaws.com:5432/d9blbach5en55p"
 SQALCHEMY_TRACK_MODIFICATIONS = False
