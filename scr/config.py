6bff2e8f669ffb054d64934367c91c3359d4a46a
Pedro <phenrique.alvesilva@gmail.com>
2022-02-14 11:01:03 -0300

    connectDB

--- scr/config.py
+++ scr/config.py
@@ -1,3 +1,11 @@
+import os
+import re
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
 SECRET_KEY = "secreto"
-SQLALCHEMY_DATABASE_URI = "postgres://sbcrkyprfsgwmv:9a1aa2b938b5ecec2a48e1bea802fa7e14ee92e8668bd2e5528572543792b32c@ec2-34-206-148-196.compute-1.amazonaws.com:5432/d9btqakpo2re4o"
+SQLALCHEMY_DATABASE_URI = uri
 SQALCHEMY_TRACK_MODIFICATIONS = False
